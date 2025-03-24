7ec582be2d65f0330c70c9468fe6737fc5e2328f
kurt <trainee.kurt.martin-brown@sigmalabs.co.uk>
2025-03-24 17:51:29 +0000

    edit labels button now displays available labels on clicka and allows for custom label creation

--- keepsake-app/src/App.tsx
+++ keepsake-app/src/App.tsx
@@ -1,15 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Gallery from "./components/Gallery";
 
+export interface label {
+  labelID: number;
+  userID: number;
+  labelName: string;
+}
+
 function App() {
+  const [labels, setLabels] = useState<label[]>([]);
+  useEffect(() => {
+    const fetchLabels = async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/labels`);
+        if (!res.ok) throw new Error("Failed to fetch labels");
+        const data = await res.json();
+        setLabels(data);
+      } catch (err) {
+        console.error("Error fetching labels:", err);
+      }
+    };
+
+    fetchLabels();
+  }, []);
   return (
     <>
       <Navbar />
       <div className="sidebar-gallery">
-        <Sidebar />
+        <Sidebar labels={labels} setLabels={setLabels} />
         <Gallery />
       </div>
     </>
--- keepsake-app/src/components/Sidebar.tsx
+++ keepsake-app/src/components/Sidebar.tsx
@@ -1,10 +1,76 @@
 import React from "react";
 import "./Sidebar.css";
+import { useState, useEffect } from "react";
+import { label } from "../App.tsx";
 
-export default function Sidebar() {
+interface sidebarProps {
+  labels: label[];
+  setLabels: React.Dispatch<any>;
+}
+
+export default function Sidebar({ labels, setLabels }: sidebarProps) {
+  const [isEditLabels, setIsEditLabels] = useState(false);
+
+  function handleEditLabelsClick() {
+    setIsEditLabels(true);
+  }
+
+  const Input = () => {
+    const [inputValue, setInputValue] = useState("");
+
+    const handleKeyDown = async (
+      event: React.KeyboardEvent<HTMLInputElement>
+    ) => {
+      if (event.key === "Enter" && inputValue.trim() !== "") {
+        try {
+          const newLabel = { labelName: inputValue };
+          const res = await fetch(`http://localhost:3000/labels`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newLabel),
+          });
+
+          if (!res.ok) throw new Error("Failed to post label");
+          const data = await res.json();
+          setLabels((prev: label[]) => [...prev, data]); //our post request only returns the new label not the list of labels
+          setInputValue("");
+          setIsEditLabels(false);
+        } catch (err) {
+          console.error("Error posting label:", err);
+        }
+      }
+    };
+
+    return (
+      <label>
+        Custom label:
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+      </label>
+    );
+  };
+
   return (
     <div className="sidebar">
-      <button className="edit-labels-btn">Edit Labels</button>
+      <button className="edit-labels-btn" onClick={handleEditLabelsClick}>
+        Edit Labels
+      </button>
+      {isEditLabels && (
+        <>
+          <Input />
+          <select>
+            {labels.map((label, idx) => (
+              <option key={idx} value={label.labelName}>
+                {label.labelName}
+              </option>
+            ))}
+          </select>
+        </>
+      )}
       <button className="add-notes-btn">Add Note</button>
       <section className="login">
         <h2>Login</h2>

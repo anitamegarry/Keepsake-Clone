20f63376b8b8a0d935f639fec5c79db6ba82e844
Anita Megarry <anitamegarry@gmail.com>
2025-03-27 16:07:50 +0000

    Created AI query

---
+++ packages/keepsake-app/src/components/SemanticLabel.tsx
@@ -0,0 +1,34 @@
+import { a } from "vitest/dist/chunks/suite.d.FvehnV49.js";
+
+
+const AIprompt = "Give a one word answer to categorise the following note: "
+const APIurl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.API_KEY}`
+console.log(APIurl)
+
+async function postAIQuery(title: string, content: string) {
+    // returns the response of an AI query
+    const response = await fetch(APIurl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            contents: [{
+                parts: [{ text: `${AIprompt} note title: ${title}, note content: ${content}` }]
+            }]
+        }),
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to connect to Gemini");
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+
+export function getSemanticLabel() {
+    const AIresponse = postAIQuery("groceries", "apples")
+    console.log(AIresponse)
+}
